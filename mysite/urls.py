40fc095f5f2bb8bc599e830dd05971443cad3228
Manas <manaskishore16@gmail.com>
2022-01-02 15:13:59 +0530

    commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 
 admin.site.site_header = "MS ENTERPRISES PORTAL"
 admin.site.site_title = "MS ENTERPRISES Admin Portal"
@@ -27,7 +25,7 @@ admin.site.index_title = "Welcome to MS ENTERPRISES Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
