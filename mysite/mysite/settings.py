fd99eab3362c3b9d5bde62a5ee54f081b7b8c1a7
Manas <manaskishore16@gmail.com>
2022-01-02 22:39:35 +0530

    commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1mm5gu)cn+89*^#ap7x*z-58c1e$u4m#z1m(z$9%vj6#%k+u_c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'msenterprises.herokuapp.com']
 
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #added manually
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static"),
+    os.path.join(BASE_DIR / "static")
 ]
 
 # Default primary key field type
